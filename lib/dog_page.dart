16618bb67036bd5cd9bf3797a1d44360d0fb0f69
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-02-02 15:35:55 -0600

    added dog_page.dart

---
+++ lib/dog_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import './dog.dart';
+
+class DogPage extends StatelessWidget {
+  static const route = 'dog';
+
+  final Dog dog;
+
+  const DogPage({required this.dog, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final description = breedDescriptions[dog.breed] ?? 'none';
+    const imageSize = 200.0;
+    final image = SizedBox(
+      child: Image.asset('assets/images/${dog.photoFileName}'),
+      height: imageSize,
+      width: imageSize,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(dog.name),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              image,
+              SizedBox(height: 20),
+              Text(dog.breed,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(height: 20),
+              Text(description),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
